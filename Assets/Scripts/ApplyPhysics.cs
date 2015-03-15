653ab78912c5dad7a67bca82a12cfb1371b6fe18
victor <cfk.infinite@gmail.com>
2015-03-15 13:54:03 +0800

    Commit to ensure that keys and mock touch can be used on the editor. Engine particles and trails updated as well.

--- Assets/Scripts/ApplyPhysics.cs
+++ Assets/Scripts/ApplyPhysics.cs
@@ -6,54 +6,65 @@ public class ApplyPhysics : MonoBehaviour {
     public KeyCode FwdAccCode;
 
     public GameObject Track;
-
     public GameObject thingToAccelerate;
 
     [HideInInspector, System.NonSerialized]
     public bool Accelerate;
 
     public ParticleSystem psys;
+	public float pSysMinEmission = 3;
+	public float pSysMaxEmission = 20;
+
     public TrailRenderer trail;
+	public float trailMinWidth = 0.05f;
+	public float trailMaxWidth = 1;
 
     public float accVal = 75;
     public float normalizedVal = 0;
+
+	public ApplyPhysics otherApplyPhysics;
+
+	[HideInInspector, System.NonSerialized]
+	public bool usingTouch;
 
 //    public float rotaVal = 10f;
 //	// Use this for initialization
 	void Start () {
 		normalizedVal =0;
+		usingTouch = true;
 	}
 //	
 	// Update is called once per frame
 	void FixedUpdate () {
-	
-		if(normalizedVal>0 || Input.GetKey(FwdAccCode))//(Input.GetKey(FwdAccCode))
+
+		if(otherApplyPhysics && Mathf.Abs(otherApplyPhysics.normalizedVal-normalizedVal)<0.05f )
+		{
+			otherApplyPhysics.normalizedVal = normalizedVal = (otherApplyPhysics.normalizedVal+normalizedVal)/2;
+		}
+
+		if(Input.GetKey(FwdAccCode)) usingTouch = true;
+		if(usingTouch)
+		{
+			if(Input.GetKey(FwdAccCode))	normalizedVal =1;
+			else normalizedVal =0;
+		}
+
+		if(normalizedVal > 0 )//(Input.GetKey(FwdAccCode))
         {
-            if(Input.GetKey(FwdAccCode))	normalizedVal =1;
-
-            if(psys)	psys.emissionRate = 15;
-
-            if(trail)
-            {
-                trail.startWidth = 0.5f;
-                trail.endWidth = 0.5f;
-            }
-
-            thingToAccelerate.rigidbody.AddForceAtPosition(Track.transform.forward* accVal * normalizedVal,Track.transform.position);
+			thingToAccelerate.rigidbody.AddForceAtPosition(Track.transform.forward* accVal * normalizedVal,Track.transform.position);
         }
         else
         {
-            if(psys)
-                psys.emissionRate = 4;
-
-            if(trail)
-            {
-                trail.startWidth = 0.1f;
-                trail.endWidth = 0.1f;
-            }
-
             thingToAccelerate.rigidbody.AddForceAtPosition(Track.transform.forward*5,Track.transform.position);
         }
+
+		if(psys)	psys.emissionRate = normalizedVal*(pSysMaxEmission - pSysMinEmission) + pSysMinEmission;
+		
+		if(trail)
+		{
+			trail.startWidth = normalizedVal*(trailMaxWidth - trailMinWidth) + trailMinWidth;
+			trail.endWidth = normalizedVal*(trailMaxWidth - trailMinWidth) + trailMinWidth;
+		}
 
 //        if(Input.GetKey(BckAccCode))
 //        {
