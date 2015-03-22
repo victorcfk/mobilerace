a4c0601f1092ffdf4136f8d6f234e6af633a7daa
victor <cfk.infinite@gmail.com>
2015-03-22 18:53:41 +0800

    Imported the trackbuildr plugin

---
+++ Assets/TrackBuildR/Scripts/Utils/Editor/ExportMaterial.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TrackBuildRUtil
+{
+    public struct ExportMaterial
+    {
+        public string name;
+        public Material material;
+        public string filepath;
+        public bool generated;
+    }
+}
