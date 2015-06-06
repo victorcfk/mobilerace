d35ae2d2c3baa6d2eaa2e32cabf5dc5ffd192b95
victor <cfk.infinite@gmail.com>
2015-06-06 22:55:22 +0800

    Added the vehicle rotate on turn aesthetics and supporting scripts.

---
+++ Assets/Scripts/VehRotateOnTurn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehRotateOnTurn : MonoBehaviour {
+
+    public DrivingScriptStraight DS;
+    public GameObject VehicleCore;
+    public float rotateSpeed= 120;
+
+    float TurnValue;
+
+
+    public float MaxTurnAngle = 90;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+       
+        if(DS == null) return;
+
+        TurnValue = DS.LeftRightAcc;
+
+        transform.localRotation = 
+            Quaternion.RotateTowards( transform.localRotation,
+                                     Quaternion.AngleAxis(TurnValue*-MaxTurnAngle,Vector3.forward),Time.deltaTime*rotateSpeed);
+	}
+}
