8619561514682b73acca38a35bfffd854a2bf5f3
victor <cfk.infinite@gmail.com>
2015-09-24 22:23:13 +0800

    Added a number of custom properties

---
+++ Assets/CustomProperties/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyAttributeDrawer: PropertyDrawer
+{
+
+    private ReadOnlyAttribute _attributeValue = null;
+    private ReadOnlyAttribute attributeValue
+    {
+        get
+        {
+            if (_attributeValue == null)
+            {
+                _attributeValue = (ReadOnlyAttribute) attribute;
+            }
+            return _attributeValue;
+        }
+    }
+
+
+    public override float GetPropertyHeight(SerializedProperty property,
+                                            GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+    
+    public override void OnGUI(Rect position,
+                               SerializedProperty property,
+                               GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
